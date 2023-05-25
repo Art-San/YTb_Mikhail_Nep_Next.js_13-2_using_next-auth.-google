e6eb390b23aed790eb3cb2f073e78738964f602d
Aleksander <asava@sibmail.com>
2023-05-25 23:09:04 +0700

    поиск сделан на клиенте

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,35 +1,31 @@
-import { Metadata } from "next";
-import Link from "next/link";
+'use client'
+import Posts from '@/components/Posts'
+import PostSearch from '@/components/PostSearch'
+import { getAllPosts } from '@/services/getPosts'
+import { Metadata } from 'next'
 
-async function getData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    next: {
-      revalidate: 60,
-    },
-  });
+import { useEffect, useState } from 'react'
 
-  if (!response.ok) throw new Error("Unable to fetch posts!");
-
-  return response.json();
-}
+// так как это теперь клиентский компонент, мы теперь не можем использовать async/await
 
 export const metadata: Metadata = {
-  title: "Blog | Next App",
-};
+  title: 'Blog | Next App'
+}
 
-export default async function Blog() {
-  const posts = await getData();
-
+export default function Blog() {
+  const [posts, setPosts] = useState<any[]>([])
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    getAllPosts()
+      .then(setPosts)
+      // .then((data) => setPosts(data))
+      .finally(() => setLoading(false))
+  }, [])
   return (
     <>
       <h1>Blog page</h1>
-      <ul>
-        {posts.map((post: any) => (
-          <li key={post.id}>
-            <Link href={`/blog/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <PostSearch onSearch={setPosts} />
+      {loading ? <h3>Loading...</h3> : <Posts posts={posts} />}
     </>
-  );
+  )
 }
