b50f1aecc6b9e95b8dff49252276c3cdf3c909c3
Aleksander <asava@sibmail.com>
2023-05-26 22:31:15 +0700

    zustand

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,31 +1,27 @@
-'use client'
 import Posts from '@/components/Posts'
 import PostSearch from '@/components/PostSearch'
-import { getAllPosts } from '@/services/getPosts'
 import { Metadata } from 'next'
-
-import { useEffect, useState } from 'react'
-
-// так как это теперь клиентский компонент, мы теперь не можем использовать async/await
 
 export const metadata: Metadata = {
   title: 'Blog | Next App'
 }
 
 export default function Blog() {
-  const [posts, setPosts] = useState<any[]>([])
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    getAllPosts()
-      .then(setPosts)
-      // .then((data) => setPosts(data))
-      .finally(() => setLoading(false))
-  }, [])
+  // нам больше не понадобится эти сущности
+  // const [posts, setPosts] = useState<any[]>([])
+  // const [loading, setLoading] = useState(true)
+  // useEffect(() => {
+  //   getAllPosts()
+  //     .then(setPosts)
+  //     // .then((data) => setPosts(data))
+  //     .finally(() => setLoading(false))
+  // }, [])
+
   return (
     <>
       <h1>Blog page</h1>
-      <PostSearch onSearch={setPosts} />
-      {loading ? <h3>Loading...</h3> : <Posts posts={posts} />}
+      <PostSearch />
+      <Posts />
     </>
   )
 }
--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,10 +1,21 @@
+'use client'
+import { usePosts } from '@/store'
 import Link from 'next/link'
-type Props = {
-  posts: any[]
-}
+import { useEffect } from 'react'
+import { shallow } from 'zustand/shallow'
 
-const Posts = ({ posts }: Props) => {
-  return (
+const Posts = () => {
+  const [posts, loading, getAllPosts] = usePosts(
+    (state) => [state.posts, state.loading, state.getAllPosts],
+    shallow // shallow чтобы лишний раз не перерэндорировалась
+  )
+
+  useEffect(() => {
+    getAllPosts()
+  }, [getAllPosts])
+  return loading ? (
+    <h3>Loading...</h3>
+  ) : (
     <ul>
       {posts.map((post: any) => (
         <li key={post.id}>
