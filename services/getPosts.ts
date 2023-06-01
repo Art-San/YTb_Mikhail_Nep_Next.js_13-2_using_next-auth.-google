56cb6f00c4b4c0a61867c1823803b8226abe4738
Aleksander <asava@sibmail.com>
2023-06-01 20:20:16 +0700

    ok

--- services/getPosts.ts
+++ services/getPosts.ts
@@ -1,17 +1,19 @@
 export const getAllPosts = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const response = await fetch('/api/posts')
+  // const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-  if (!response.ok) throw new Error("Unable to fetch posts.");
+  if (!response.ok) throw new Error('Unable to fetch posts.')
 
-  return response.json();
-};
+  return response.json()
+}
 
 export const getPostsBySearch = async (search: string) => {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?q=${search}`
-  );
+    `/api/posts?q=${search}`
+    // `https://jsonplaceholder.typicode.com/posts?q=${search}`
+  )
 
-  if (!response.ok) throw new Error("Unable to fetch posts.");
+  if (!response.ok) throw new Error('Unable to fetch posts.')
 
-  return response.json();
-};
+  return response.json()
+}
