03b9d6666e27e168b1f178dad4f753c307c54a79
Art San <artsan.mcc@gmail.com>
2024-01-19 23:44:24 +0700

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import Image from 'next/image'
+import styles from './pageHome.module.css'
+import Img from './assets/sportivnyj-bmw.jpg'
+
 export default function Home() {
-  return <h1>Welcome to NextJS world</h1>;
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to NextJS world</h1>
+      <Image
+        src={Img}
+        alt="sportCar"
+        // width={600} // если вот так src={Img} то width height не нужны
+        // height={500}
+        priority // значит приоритет у картинки
+      />
+      {/* <Image
+        src={'https://avatarzo.ru/wp-content/uploads/derevo-na-zakate.jpg'}
+        alt="sportCar"
+        width={600} // если вот так src={Img} то width height не нужны
+        height={500}
+        priority // значит приоритет у картинки
+      /> */}
+    </div>
+  )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    // formats: ['image/avif', 'image/webp'],
+    // deviceSizes: [375, 640, 750, 928, 1000, 1200, 1920],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatarzo.ru',
+        port: '',
+        pathname: ''
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
+
+// https://avatarzo.ru/wp-content/uploads/derevo-na-zakate.jpg
